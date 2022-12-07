648b70ed8bb94bb474606871176b83e6d7d854f5
WMXPY <wm@mengw.io>
2022-12-06 23:05:26 -0800

    add mount monaco editor

---
+++ src/editor/monaco.ts
@@ -0,0 +1,27 @@
+/**
+ * @author WMXPY
+ * @namespace Editor
+ * @description Monaco
+ */
+
+import { Monaco } from "@monaco-editor/react";
+import { markedDateMonacoMixinFactory } from "@sudoo/marked-mixin-date-monaco";
+import { markedJsonMonacoMixinFactory } from "@sudoo/marked-mixin-json-monaco";
+import { markedMathMonacoMixinFactory } from "@sudoo/marked-mixin-math-monaco";
+import { markedObjectMonacoMixinFactory } from "@sudoo/marked-mixin-object-monaco";
+import { markedParseMonacoMixinFactory } from "@sudoo/marked-mixin-parse-monaco";
+import { MarkedMonacoManager } from "@sudoo/marked-monaco";
+
+export const mountMonacoEditor = async (monaco: Monaco): Promise<void> => {
+
+    const markedMonacoManager: MarkedMonacoManager =
+        MarkedMonacoManager.createAndMountBase(monaco, 'javascript');
+
+    markedMonacoManager.use(markedDateMonacoMixinFactory.createInjectMixin("Date"));
+    markedMonacoManager.use(markedJsonMonacoMixinFactory.createInjectMixin("Json"));
+    markedMonacoManager.use(markedMathMonacoMixinFactory.createInjectMixin("Math"));
+    markedMonacoManager.use(markedObjectMonacoMixinFactory.createInjectMixin("Object"));
+    markedMonacoManager.use(markedParseMonacoMixinFactory.createInjectMixin("Parse"));
+
+    return;
+};
