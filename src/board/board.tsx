bcde8a5b9bff793e2d898fe414c1d8123ba15f12
wmxpy <wm@mengw.io>
2022-12-14 19:15:12 -0600

    init board

---
+++ src/board/board.tsx
@@ -0,0 +1,14 @@
+/**
+ * @author WMXPY
+ * @namespace Board
+ * @description Board
+ */
+
+import React from "react";
+
+export const BoardView: React.FC = () => {
+
+    return (<div>
+        Board
+    </div>);
+};
